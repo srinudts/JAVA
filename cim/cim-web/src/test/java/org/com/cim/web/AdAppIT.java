610c91d7ca855c557f02c39db89347d27a42c5cc
ETS\SBODDAPATI <srinudts@gmail.com>
2017-06-25 21:18:35 -0400

    Update the integration test

--- cim/cim-web/src/test/java/org/com/cim/web/AdAppIT.java
+++ cim/cim-web/src/test/java/org/com/cim/web/AdAppIT.java
@@ -2,16 +2,24 @@ package org.com.cim.web;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.com.cim.common.dto.AdDto;
 import org.com.cim.common.form.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -36,10 +44,56 @@ public class AdAppIT {
 	private TestRestTemplate restTemplate;
 
 	@Test
+	public void testFindAdByPartnerId1() {
+
+
+		JSONObject request = new JSONObject();
+		try {
+			request.put("partnerId", "p1");
+			request.put("duration", 10);
+			request.put("adContent", "TEST");
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+
+		// set headers
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<String> entity = new HttpEntity<String>(request.toString(), headers);
+
+		// send request and parse result
+		ResponseEntity<String> loginResponse = restTemplate.exchange("/ad/", HttpMethod.POST, entity, String.class);
+		
+		if (loginResponse.getStatusCode() == HttpStatus.OK) {
+		  try {
+			JSONObject userJson = new JSONObject(loginResponse.getBody());
+			System.out.println(userJson);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		} else if (loginResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+			
+		  // nono... bad credentials
+		}
+		
+	}
+	
+	@Test
 	public void testFindAdByPartnerId() {
-		ResponseEntity<Response> responseEntity = this.restTemplate.getForEntity("/ad", Response.class);
-		List<AdDto> adDto =(List<AdDto>)responseEntity.getBody().getData();
-		assertEquals(false, adDto.get(0).getActvFlag());
+		ResponseEntity<Response> responseEntity = this.restTemplate.getForEntity("/ad/p1", Response.class);
+		LinkedHashMap<String, String> lh = (LinkedHashMap<String, String>) responseEntity.getBody().getData();
+		System.out.println(lh.keySet()+", "+ lh.values());
+		assertEquals("p1", lh.get("partnerId"));
+		
+	}
+	
+	
+	@Test
+	public void testFindAdByPartnerId2() {
+		ResponseEntity<Response> responseEntity = this.restTemplate.getForEntity("/ad/p1", Response.class);
+		LinkedHashMap<String, String> lh = (LinkedHashMap<String, String>) responseEntity.getBody().getData();
+		System.out.println(lh.keySet()+", "+ lh.values());
+		assertEquals("p1", lh.get("partnerId"));
 		
 	}
 
